34905166635745f3d951c32461685b5498c667eb
lsr90518 <lb90518@gmail.com>
2015-03-12 09:33:26 +0900

    save to database

--- routes/index.js
+++ routes/index.js
@@ -12,21 +12,36 @@ router.get('/', function (req, res) {
 
     //根据关键字，暧昧搜索，获得搜索结果链接，详细内容。
 //    for (var i = 1;i<2;i++){
-        prodAdv.call("ItemSearch", {BrowseNode:"465610",SearchIndex:"Books",ResponseGroup:"SalesRank",Sort:"salesrank",ItemPage:"2"}, function(err, result) {
+        prodAdv.call("ItemSearch", {BrowseNode:"465610",SearchIndex:"Books",ResponseGroup:"SalesRank",Sort:"salesrank",ItemPage:"3"}, function(err, result) {
 
             console.log(err);
             console.log(result);
+            //init database
+            var server = new mongodb.Server("104.155.213.128",80,{safe:true});
+
             var items = result.Items.Item;
-//            console.log(result.BrowseNodes.BrowseNode.TopSellers);
-//            console.log(result.BrowseNodes.BrowseNode.TopItemSet);
+
+            new mongodb.Db('dawn',server,{}).open(function(error,client){
+                if(error) throw error;
+
+                var collection = new mongodb.Collection(client,'amazon');
+
+                //insert data
+                collection.insert(items,function(err,result){
+                    console.log(err);
+                });
+
+            });
+
             for(var i = 0;i<items.length;i++){
                 console.log(items[i]);
             }
+
         });
 //    }
 
     //根据ID 搜索详细，获取单个商品详细内容。
-//    prodAdv.call("ItemLookup", {ItemId: "4121023005", ResponseGroup:"SalesRank"}, function(err, result) {
+//    prodAdv.call("ItemLookup", {ItemId: "4121023005", ResponseGroup:"ItemAttributes"}, function(err, result) {
 //
 //        console.log(err);
 //        console.log(result.Items.Item);
@@ -45,14 +60,31 @@ router.get('/test', function (req, res) {
     var server = new mongodb.Server("104.155.213.128",80,{safe:true});
     new mongodb.Db('dawn',server,{}).open(function(error,client){
         if(error) throw error;
-        var collection = new mongodb.Collection(client,'user');
+
+        var collection = new mongodb.Collection(client,'amazon');
+        var data = [{"name":"2"},{"name":"3"}];
+        var removeData = {"x":3};
+        //insert data
+        collection.insert(data,function(err,result){
+            console.log(err);
+        });
+
+
+        //remove data
+//        collection.remove(removeData,function(err,result){
+//            console.log(err);
+//        });
+
+        //search data
         collection.find(function(error,cursor){
+
             cursor.each(function(error,doc){
                 if(doc){
                     console.log(doc);
                 }
             });
         });
+
     });
 
 
